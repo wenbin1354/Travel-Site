a68ff91bc8242a05013f4f680401ba6663ddc8f2
WenBin Lin <wblin2986@gmail.com>
2023-03-05 00:42:22 -0500

    added styling and content for home page

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,39 +1,54 @@
-import { Outlet, Link } from "react-router-dom";
+import { Outlet, Link, useLocation } from "react-router-dom";
 import "./Navbar.css";
 
 function Navbar() {
+	let location = useLocation().pathname;
+
 	return (
 		<div>
 			<nav>
 				<ul>
-					<li style={{marginTop:"-10px"}}>
+					<li style={{ marginTop: "-10px" }}>
 						<Link className="nav-title" to="/">
 							张家界
 						</Link>
 					</li>
 					<li>
-						<Link className="nav-button" to="/">
+						<Link
+							className="nav-button"
+							to="/"
+							style={{ color: location === "/" && "rgb(28, 198, 28)" }}
+						>
 							Home
 						</Link>
 					</li>
 					<li>
-						<Link className="nav-button" to="/lodging">
+						<Link
+							className="nav-button"
+							to="/lodging"
+							style={{ color: location === "/lodging" && "rgb(28, 198, 28)" }}
+						>
 							Lodging
 						</Link>
 					</li>
 					<li>
-						<Link className="nav-button" to="/foods">
+						<Link
+							className="nav-button"
+							to="/foods"
+							style={{ color: location === "/foods" && "rgb(28, 198, 28)" }}
+						>
 							Foods
 						</Link>
 					</li>
 					<li>
-						<Link className="nav-button" to="/attractions">
+						<Link
+							className="nav-button"
+							to="/attractions"
+							style={{
+								color: location === "/attractions" && "rgb(28, 198, 28)",
+							}}
+						>
 							Attractions
-						</Link>
-					</li>
-					<li>
-						<Link className="nav-button" to="/nothing-here">
-							Nothing Here
 						</Link>
 					</li>
 				</ul>
